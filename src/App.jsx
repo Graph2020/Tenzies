5b217815651d1b554984e6023120334b480098c6
Jacopo <163671349+Graph2020@users.noreply.github.com>
2026-03-29 16:47:01 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen w-full bg-[#0B2434]">
+      <h1 className="text-3xl text-amber-300">Hi</h1>
+    </main>
+  );
+}
+
+export default App;
